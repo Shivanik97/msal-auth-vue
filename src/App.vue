bb710219488d75292405adfef556ad6f1b878127
Shivani K <shivanik.bbi18@rvce.edu.in>
2024-06-25 12:45:50 +0530

    removed unnecessary code

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <div v-if="state.isAuthenticated">
       <div>Welcome, {{ state.user?.name }}!</div>
       <button @click="handleLogout">Log Out</button>
-      <RouterLink to="/about">About</RouterLink>
     </div>
     <div v-else>
       <button @click="handleLogin">Log In</button>
@@ -12,34 +11,10 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import axios from 'axios'
+import { onMounted } from 'vue'
 import { msalService } from './config/useAuth'
 import { msalInstance, state } from './config/msalConfig'
-
-interface GetAllFeatureFlags {
-  name: string
-  description: string
-  team: string
-  environmentProperties: {
-    [key: string]: {
-      isEnabled: boolean
-      /* eslint-disable */
-      client_filters: ClientFilters[] | null
-    }
-  }
-}
-interface ClientFilters {
-  name: string
-  parameters: {
-    audience: {
-      groups: { Name: string }[] /* eslint-disable */
-    }
-  }
-}
 const { login, logout, handleRedirect, registerAuthorizationHeaderInterceptor } = msalService()
-const isLoading = ref(false)
-const featureFlags = ref<GetAllFeatureFlags[]>([])
 
 const handleLogin = async () => {
   await login()
@@ -57,22 +32,8 @@ const initialize = async () => {
     console.log('Initialization error', error)
   }
 }
-const getAllFeatures = async () => {
-  try {
-    isLoading.value = true
-    const response = await axios.get(
-      'https://feature-state-tool-dev.azurewebsites.net/FeatureFlag/FeatureFlags'
-    )
-    featureFlags.value = response.data
-  } catch (error) {
-    console.error('Error fetching feature flags:', error)
-  } finally {
-    isLoading.value = false
-  }
-}
 onMounted(async () => {
   await initialize()
   await handleRedirect()
-  await getAllFeatures()
 })
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import { msalInstance } from '@/config/msalConfig'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,21 +12,8 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      component: () => import('../views/AboutView.vue'),
-      meta: { requiresAdmin: true }
+      component: () => import('../views/AboutView.vue')
     }
   ]
-})
-router.beforeEach(async (to, from) => {
-  const accounts = msalInstance.getAllAccounts()
-  if (to.matched.some((record) => record.meta.requiresAdmin)) {
-    const userHasAdminRole = accounts.some((account) =>
-      account.idTokenClaims?.roles?.includes('Enable.PO')
-    )
-    if (!userHasAdminRole) {
-      return { name: 'login' }
-    }
-  }
-  return true
 })
 export default router
